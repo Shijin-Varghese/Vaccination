71258be3ee51f732bae9860c5c137d3ea4249e19
Kunal Jindal <kunaljindal@mnnit.ac.in>
2023-05-17 23:05:19 +0530

    Complete Appointment, added Swagger and mail sender

--- src/main/java/com/example/Dosify/service/impl/Dose1ServiceImpl.java
+++ src/main/java/com/example/Dosify/service/impl/Dose1ServiceImpl.java
@@ -4,9 +4,11 @@ import com.example.Dosify.Enum.VaccineType;
 import com.example.Dosify.model.Dose1;
 import com.example.Dosify.model.User;
 import com.example.Dosify.service.Dose1Service;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public class Dose1ServiceImpl implements Dose1Service {
 
     @Override
